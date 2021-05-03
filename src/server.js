407896ea13dc5b0edf8ef228295a7cf91f382f20
Jreyesh26 <83368766+Jreyesh26@users.noreply.github.com>
2021-05-02 22:56:37 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,11 +9,18 @@ const routes = require('./route');
 //settings
 app.set('port', process.env.PORT || 3000);
 const dbOptions = {
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password:'',
-    database:'productodb'
+  [
+{"type":"header","version":"5.1.0","comment":"Export to JSON plugin for PHPMyAdmin"},
+{"type":"database","name":"productodb"},
+{"type":"table","name":"productos","database":"productodb","data":
+[
+{"id":"1","nombre":"Celular","descripcion":"Este es un oaoaoaooaoa","precio":"1500000","cantidad":"2"},
+{"id":"2","nombre":"Tv","descripcion":"lo que sea xd","precio":"1500000","cantidad":"10"},
+{"id":"4","nombre":"Tvkk","descripcion":"lo que sea xd","precio":"1500000","cantidad":"10"},
+{"id":"5","nombre":"GEL","descripcion":"lo que sea xd","precio":"15000","cantidad":"6"}
+]
+}
+]
 };
 
 //middlewares
